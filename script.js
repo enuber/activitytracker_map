798e81e12bbcc89d32099975dd61909acf6c2900
Erik Nuber <erik.nuber@yahoo.com>
2021-03-15 09:10:40 -0700

    got map on page with a marker, updated look of map

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//got get position of user
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    () => {
+      alert('could not get your position');
+    }
+  );
+}
